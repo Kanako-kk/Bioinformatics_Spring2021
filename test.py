ceb43a40e00928777052f0e3229b002bab8ad516
Kanako Kato <83787539+Kanako-kk@users.noreply.github.com>
2021-05-06 19:23:49 -0700

    Update test.py

--- test.py
+++ test.py
@@ -16,6 +16,18 @@ print('T count is', t_count)
 print('C count is', c_count)
 print('G count is', g_count)
 
+#This function will return GC content of a sequence and writes it to a file.
+def gc_content(dna, file_object):
+    g=dna.count("G")
+    c=dna.count("C")
+    gc=(g+c)/len(dna)*100
+    print(gc)
+    file_object.write(str(gc))
+# Main part 
+my_file.open=("gc_file.txt",'w')
+gc_content(seq, my_file)
+my_file.close
+
 
 #In this part, tri nucleotide is coutned with a dictionary output. 
 tri_list=[]
